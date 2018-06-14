81f8ca94b74b866265cf157a6d58d40cf9ef9505
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-14 21:48:43 +0100

    onitemclick function :bug:

--- src/displayItems.js
+++ src/displayItems.js
@@ -2,19 +2,52 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-14 14:39:22 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-14 14:46:43
+ * @Last Modified time: 2018-06-14 21:48:25
  */
 
-import React from "react";
+import React, { Component } from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import PropTypes from 'prop-types';
 
-/*
- * users details 
- */
-export default (DisplayItems = props => (
-  <View>
-    <TouchableOpacity onPress={() => { Alert.alert("dsdqs"); }} >
-      
-    </TouchableOpacity>
-  </View>
-));
+
+export default class DisplayItems extends Component {
+    /*  static propTypes = {
+        onItemClick: PropTypes.func
+    }
+    
+    static defaultProps = {
+        onItemClick: () => Alert.alert('item Clicked'),
+    }*/
+
+    /*
+    * details 
+    */
+    _renderElement = (item, itemsStyles) => {
+        console.log('item: ' + item)
+        let elements = []
+        Object.entries(item).forEach(([key, value]) => {
+            let delta = new Date().getTime()
+            if (key !== 'key')
+                elements.push(<Text style={itemsStyles[key]} >{item[key]}</Text>)
+        })
+        if (true)
+            return elements
+        return null
+    }
+    onPress = () => {
+        //this.props.onItemClick()
+        console.log('clicked');
+    }
+    render() {
+        return (
+            <TouchableOpacity onPress={this.onPress} >
+                <View style={this.props.itemsStyles.flatview} >
+                    {this._renderElement(this.props.item, this.props.itemsStyles)}
+                </View>
+            </TouchableOpacity>
+        )
+
+
+    }
+
+}
