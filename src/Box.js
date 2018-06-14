04c9ce1cc36b44dcf65b647e222fde87579e0a1f
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-14 04:19:05 +0100

    keyextractor bug :beetle:

--- src/Box.js
+++ src/Box.js
@@ -2,7 +2,7 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-13 22:55:47 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-14 04:03:08
+ * @Last Modified time: 2018-06-14 04:18:18
  */
 import React, { Component } from 'react';
 import {
@@ -46,6 +46,10 @@ export default class Box extends Component {
         return null;
     }
 
+    _keyExtractor = (item, index) => {
+        console.log(JSON.stringify(item))
+        item.key
+    };
 
     render() {
         return (
@@ -54,15 +58,11 @@ export default class Box extends Component {
                     <FlatList
                         data={this.props.data}
                         showsVerticalScrollIndicator={false}
-                        renderItem={({ item }) => {
-                            return (
-                                <View style={this.props.itemsStyles.flatview}>
-                                    {this.renderElement(item, this.props.itemsStyles)}
-                                </View>
-                            )
+                        renderItem={({ item }) =>
+                            <View style={this.props.itemsStyles.flatview}>
+                                {this.renderElement(item, this.props.itemsStyles)}
+                            </View>
                         }
-                        }
-                        keyExtractor={(item, index) => item.key}
                     />
                 </View>
             </ScrollView>
