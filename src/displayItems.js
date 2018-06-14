1ec2196a78396066ff11658b68cb964793ffac83
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-14 14:46:55 +0100

    new file

---
+++ src/displayItems.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: kerim selmi 
+ * @Date: 2018-06-14 14:39:22 
+ * @Last Modified by: kerim selmi
+ * @Last Modified time: 2018-06-14 14:46:43
+ */
+
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity, Alert } from "react-native";
+
+/*
+ * users details 
+ */
+export default (DisplayItems = props => (
+  <View>
+    <TouchableOpacity onPress={() => { Alert.alert("dsdqs"); }} >
+      
+    </TouchableOpacity>
+  </View>
+));
