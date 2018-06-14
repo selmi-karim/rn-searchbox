9d44b50f8a87d7eb7951096915329eaac88a619a
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-14 14:46:07 +0100

    keyextractor :fixed bug:

--- src/Box.js
+++ src/Box.js
@@ -2,48 +2,82 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-13 22:55:47 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-14 04:39:40
+ * @Last Modified time: 2018-06-14 14:45:39
  */
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
     Text,
     View,
     FlatList,
     StyleSheet,
-    ScrollView
-} from 'react-native';
-import PropTypes from 'prop-types';
+    ScrollView,
+    TouchableOpacity
+} from 'react-native'
+import PropTypes from 'prop-types'
+import DisplayItems from './displayItems'
 
 export default class Box extends Component {
     static propTypes = {
         data: PropTypes.array,
         input: PropTypes.string,
-    };
+    }
 
     static defaultProps = {
         data: [],
         input: '',
-    };
-
-    renderElement(item, itemsStyles) {
-        let elements = []
-        Object.entries(item).forEach(([key, value]) => {
-            if (key !== 'key')
-               elements.push(<Text key={item['key']} style={itemsStyles[key]} >{item[key]}</Text>)
-        });
-        if (true)
-            return (
-                <View>
-                    {elements}
-                </View >
-            );
-        return null;
     }
 
+    _renderElement = (item, itemsStyles) => {
+        let elements = []
+        Object.entries(item).forEach(([key, value]) => {
+            let delta = new Date().getTime()
+            if (key !== 'key')
+                elements.push(<Text  style={itemsStyles[key]} >{item[key]}</Text>)
+        })
+        if (true)
+            return elements
+        return null
+    }
+    _generateUniqueID = () => {
+        return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)+ Date.now().toString()).toUpperCase();
+    }
     _keyExtractor = (item, index) => {
-        console.log(JSON.stringify(item))
-        item.key
-    };
+        return index.toString(
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        )
+    }
+    _onPressItem = (item) => {
+        // updater functions are preferred for transactional updates
+        console.log('click: ' + JSON.stringify(item))
+    }
+    _renderItem = ({ item }) => {
+        return (
+            <View style={this.props.itemsStyles.flatview} >
+                {this._renderElement(item, this.props.itemsStyles)}
+            </View>)
+
+    }
+
+    /** show when no result */
+    _renderEmpty = () => (
+        <View style={{ height: 40, alignItems: "center", justifyContent: "center" }}>
+            <Text>No result</Text>
+        </View>
+    )
+
 
     render() {
         return (
@@ -52,16 +86,14 @@ export default class Box extends Component {
                     <FlatList
                         data={this.props.data}
                         showsVerticalScrollIndicator={false}
-                        renderItem={({ item }) =>
-                            <View style={this.props.itemsStyles.flatview}>
-                                {this.renderElement(item, this.props.itemsStyles)}
-                            </View>
-                        }
-                        keyExtractor={item => item.key}
+                        renderItem={this._renderItem}
+                        keyExtractor={this._keyExtractor}
+                        ListEmptyComponent={this._renderEmpty}
+
                     />
                 </View>
             </ScrollView>
-        );
+        )
     }
 }
 
