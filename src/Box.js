c5ecc1c23c9fa0e5f623d9e39ad2be8900503dd2
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-14 03:41:29 +0100

    renderElements fixed :beetle:

--- src/Box.js
+++ src/Box.js
@@ -2,7 +2,7 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-13 22:55:47 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-14 03:29:02
+ * @Last Modified time: 2018-06-14 03:41:26
  */
 import React, { Component } from 'react';
 import {
@@ -26,54 +26,48 @@ export default class Box extends Component {
     };
 
     renderElement(item, itemsStyles) {
-        console.log('item: ' + JSON.stringify(item))
-        console.log('itemStyles: ' + JSON.stringify(itemsStyles))
+        //console.log('item: ' + JSON.stringify(item))
+        //console.log('itemStyles: ' + JSON.stringify(itemsStyles))
         /*
         <Text style={this.props.itemsStyles.name} >{item.name}</Text>
                                     <Text style={this.props.itemsStyles.email} >{item.name}</Text>*/
 
-        Object.size = function (obj) {
-            var size = 0, key;
-            for (key in obj) {
-                if (obj.hasOwnProperty(key)) size++;
-            }
-            return size;
-        };
-        let elements = [];
-        for (let i = 0; i < Object.size(item); i++) {
-            elements.push(<Text>hello</Text>);
-        }
-        if (true)
-            return (
+        console.log('0: ' + Object.keys(item))
+        console.log('1: ' + item['name'])
+        Object.entries(item).forEach(([key, value]) => { 
+            console.log(`${key}: ${value}`);
+            return     <Text style={itemsStyles[{key}]} >{item[{key}]}</Text>
+        });
+    if(true)
+    return(
                 <View>
-                    <Text style={itemsStyles.name} >{item.name}</Text>
-                    <Text style={itemsStyles.email} >{item.email}</Text>
-                    {elements}
-                </View>
+    <Text style={itemsStyles.name} >{item.name}</Text>
+    <Text style={itemsStyles.email} >{item.email}</Text>
+                </View >
             );
-        return null;
+return null;
     }
 
-    render() {
-        return (
-            <ScrollView>
-                <View >
-                    <FlatList
-                        data={this.props.data}
-                        showsVerticalScrollIndicator={false}
-                        renderItem={({ item }) => {
-                            return (
-                                <View style={this.props.itemsStyles.flatview}>
-                                    {this.renderElement(item, this.props.itemsStyles)}
-                                </View>
-                            )
-                        }
-                        }
-                        keyExtractor={item => item.email}
-                    />
-                </View>
-            </ScrollView>
-        );
-    }
+render() {
+    return (
+        <ScrollView>
+            <View >
+                <FlatList
+                    data={this.props.data}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={this.props.itemsStyles.flatview}>
+                                {this.renderElement(item, this.props.itemsStyles)}
+                            </View>
+                        )
+                    }
+                    }
+                    keyExtractor={item => item.email}
+                />
+            </View>
+        </ScrollView>
+    );
+}
 }
 
