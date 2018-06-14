210f84776a4460af31afcac4d86a8284026bba99
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-14 03:29:10 +0100

    renderElements fixed :beetle:

--- src/Box.js
+++ src/Box.js
@@ -2,7 +2,7 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-13 22:55:47 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-14 03:15:32
+ * @Last Modified time: 2018-06-14 03:29:02
  */
 import React, { Component } from 'react';
 import {
@@ -24,17 +24,35 @@ export default class Box extends Component {
         data: [],
         input: '',
     };
-    
-    renderElement(item,itemsStyles){
-        console.log('item: '+JSON.stringify(item))
-        console.log('itemStyles: '+JSON.stringify(itemsStyles))
+
+    renderElement(item, itemsStyles) {
+        console.log('item: ' + JSON.stringify(item))
+        console.log('itemStyles: ' + JSON.stringify(itemsStyles))
         /*
         <Text style={this.props.itemsStyles.name} >{item.name}</Text>
                                     <Text style={this.props.itemsStyles.email} >{item.name}</Text>*/
-        if(true)
-           return <Text>data</Text>;
+
+        Object.size = function (obj) {
+            var size = 0, key;
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) size++;
+            }
+            return size;
+        };
+        let elements = [];
+        for (let i = 0; i < Object.size(item); i++) {
+            elements.push(<Text>hello</Text>);
+        }
+        if (true)
+            return (
+                <View>
+                    <Text style={itemsStyles.name} >{item.name}</Text>
+                    <Text style={itemsStyles.email} >{item.email}</Text>
+                    {elements}
+                </View>
+            );
         return null;
-     }
+    }
 
     render() {
         return (
@@ -46,8 +64,7 @@ export default class Box extends Component {
                         renderItem={({ item }) => {
                             return (
                                 <View style={this.props.itemsStyles.flatview}>
-                                    { this.renderElement(item,this.props.itemsStyles) }
-                                    
+                                    {this.renderElement(item, this.props.itemsStyles)}
                                 </View>
                             )
                         }
