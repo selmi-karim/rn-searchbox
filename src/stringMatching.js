9a88846ab24d76280a473aac320dc443f1c74c95
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-15 02:43:55 +0100

    dice algo :sparkles:

--- src/stringMatching.js
+++ src/stringMatching.js
@@ -2,9 +2,10 @@
  * @Author: kerim selmi 
  * @Date: 2018-06-13 22:55:50 
  * @Last Modified by: kerim selmi
- * @Last Modified time: 2018-06-15 02:42:59
+ * @Last Modified time: 2018-06-15 02:43:49
  */
 
+ 
 const diceSearch = (input, item) => {
     let itemData =""
     Object.entries(item).forEach(([key, value]) => {
